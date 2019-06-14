0530b1e1f1a1f9d72703f634228b052014a49b2a
Miroslav Lambrev <mll3218@ray26.doc.ic.ac.uk>
2019-06-14 15:58:57 +0100

    Fix bug with gpio.s (some values were assigned to the wrong registers). Remove comments as they are not yet supported in our assemble implementation. Assembly code works.

--- programs/gpio.s
+++ programs/gpio.s
@@ -1,19 +1,16 @@
-; Accessing a GPIO pin between 10 and 19
 ldr r0, =0x20200004
 mov r1, #0x40000
-str, r1, [r0] ; Makes GPIO pin 16 an output pin
+str, r1, [r0]
 mov r2, #0x10000
 loop:
-str r2, [r0, #36] ; Turns GPIO pin 16 off
-ldr r2,=0xFFFFFF
-; Doing many substraction operation to cause a delay between turning the GPIO pin on and off
+str r2, [r0, #36]
+ldr r1,=0xFFFFFF
 first_delay:
 sub r1, r1, #1
 cmp r1, #0
 bne first_delay
-str r2, [r0, #24] ; Turns GPIO pin 16 on
-ldr r2,=0xFFFFFF
-; Doing many substraction operation to cause a delay between turning the GPIO pin on and off
+str r2, [r0, #24]
+ldr r1,=0xFFFFFF
 second_delay:
 sub r1, r1, #1
 cmp r1, #0
