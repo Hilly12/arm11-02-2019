2b1ff57bc9025139a85bd6d4339db2e81f15e871
Miroslav Lambrev <mll3218@ray21.doc.ic.ac.uk>
2019-06-14 15:19:39 +0100

    Fix bug with gpio.s (now using ldr instead of mov for assigning high values)

--- programs/gpio.s
+++ programs/gpio.s
@@ -5,14 +5,14 @@ str, r1, [r0] ; Makes GPIO pin 16 an output pin
 mov r2, #0x10000
 loop:
 str r2, [r0, #36] ; Turns GPIO pin 16 off
-mov r1, #0xFFFFFF
+ldr r2,=0xFFFFFF
 ; Doing many substraction operation to cause a delay between turning the GPIO pin on and off
 first_delay:
 sub r1, r1, #1
 cmp r1, #0
 bne first_delay
 str r2, [r0, #24] ; Turns GPIO pin 16 on
-mov r2, #0xFFFFFF
+ldr r2,=0xFFFFFF
 ; Doing many substraction operation to cause a delay between turning the GPIO pin on and off
 second_delay:
 sub r1, r1, #1
