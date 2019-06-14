b18a09ff822be2092ca332662a26acea944e5745
Aahil  Mehta <aahil.mehta18@imperial.ac.uk>
2019-06-14 16:53:54 +0100

    Comment out Part II of extension

--- extension/src/run.c
+++ extension/src/run.c
@@ -10,18 +10,19 @@ int z = -1;
 char **map[MAP_WIDTH][MAP_HEIGHT];
 
 void start() {
+    /* Part II
     player_x = 0;
     player_y = 0;
     generate_map((char **) map, 5, &player_x, &player_y);
+    */
 }
 
 void update() {
-    // map[player_x - 1][player_y - 1] = (map[player_x - 1][player_y - 1]) + z;
-    // z = -z;
 }
 
 void draw() {
     fb_clear(0, SIZE);
+    /* Part II
     for (int x = 0; x < VISIBLE_WIDTH; x++) {
         for (int y = 0; y < VISIBLE_HEIGHT; y++) {
             int rx = x + player_x - HCENT;
@@ -34,8 +35,10 @@ void draw() {
             }
         }
     }
+    
 
     draw_rectangle(HCENT * SQUARE_SIZE, VCENT * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, RED);
+    */
 }
 
 void main(void) {
